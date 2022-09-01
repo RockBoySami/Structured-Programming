ccc6e28914b16e26e0fb95160071fe0e14110097
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-01 21:36:39 +0600

    Add files via upload

---
+++ lab work 8/lab_work8.c
@@ -0,0 +1,19 @@
+/*this is the C program for reversing 5 digit number*/
+#include<stdio.h>
+int main(){
+int a,b,c,d,e,num,reverse;
+printf("Enter the five digit number: ");
+scanf("%d", &num);
+
+e=num%10;
+d=(num/10)%10;
+c=(num/100)%10;
+b=(num/1000)%10;
+a=(num/10000);
+
+reverse=e*10000+d*1000+c*100+b*10+a;
+
+printf("The reversing number is: %d\n", reverse);
+
+return 0;
+}

2e609b8531eb1b41ce170729d4202f3877d9889e
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-02 11:34:22 +0600

    Add files via upload

---
+++ lab work 11/lab_work11.c
@@ -0,0 +1,22 @@
+/* Total number of currency notes of each denomination the cashier will have to give to the withdrawer*/
+#include<stdio.h>
+int main(){
+int currency,a,b,c,d,e,f;
+printf("Enter the currency in hundreds: ");
+scanf("%d", &currency);
+
+a=currency/100;
+b=currency%100;
+printf("hundred notes are: %d\n", a);
+
+c=b/50;
+d=b%50;
+printf("Fifty notes are: %d\n", c);
+
+e=d/10;
+f=d/10;
+printf("Ten notes are: %d\n", e);
+
+
+return 0;
+}

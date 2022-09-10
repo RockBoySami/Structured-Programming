a822c684145b32ca23e8f85b376fdb36ab08cb05
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-10 22:34:49 +0600

    Update work1.c

--- Simple Condition or decision making process/work 1/work1.c
+++ Simple Condition or decision making process/work 1/work1.c
@@ -1,7 +1,7 @@
 /*This is the C program for determining the profit or loss seller incurred*/
 #include<stdio.h>
 int main(){
-    float cost_price, selling_price, profit, loss;
+        float cost_price, selling_price, profit, loss;
         printf("Enter the cost price: ");
         scanf("%f",&cost_price);
 
@@ -9,13 +9,13 @@ int main(){
         scanf("%f",&selling_price);
 
         if(cost_price>selling_price){
-        
-                printf("The seller has made incurred loss and the loss is: %f\n",cost_price-selling_price);
+
+        printf("The seller has made incurred loss and the loss is: %f\n",cost_price-selling_price);
 
         }else{
 
-                printf("The seller has made profit and the profit is: %f\n",selling_price-cost_price);
+        printf("The seller has made profit and the profit is: %f\n",selling_price-cost_price);
         }
 
-return 0;
+        return 0;
         }

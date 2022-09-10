d58669d26b6883b4fe6f1f1c5179f49c6ce9b9d4
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-10 22:32:24 +0600

    Update work8.c

--- Simple Condition or decision making process/work 8/work8.c
+++ Simple Condition or decision making process/work 8/work8.c
@@ -2,16 +2,12 @@
 #include<stdio.h>
 int main(){
         float number;
-        
         printf("Enter the number: ");
         scanf("%f",&number);
-
         if (number==0){
         printf("The number is zero");
-
         }else{
         printf("The number is not zero");
         }
-
         return 0;
         }

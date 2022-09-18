69e8b75abfc813bfadc4fc9686c7de378cffc960
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-18 21:46:16 +0600

    Update work4.c

--- Complex ConditionDecision Making Practice/work4.c
+++ Complex ConditionDecision Making Practice/work4.c
@@ -5,22 +5,10 @@ int main(){
         printf("Enter the week number: ");
         scanf("%d",&week);
 
-        if(week==1){
-        printf("Saturday");
-        }else if(week==2){
-        printf("Sunday");
-        }else if(week==3){
-        printf("Monday");
-        }else if(week==4){
-        printf("Tuesday");
-        }else if(week==5){
-        printf("Wednesday");
-        }else if(week==6){
-        printf("Thrusday");
-        }else if(week==7){
-        printf("Friday");
+        if(week>=1 && week<=52){
+        printf("Saturday\nSunday\nMonday\nTuesday\nWednesday\nThrusday\nFriday\n");
         }else{
-        printf("The day is not valid");
+        printf("Invalid\n");
         }
         return 0;
         }

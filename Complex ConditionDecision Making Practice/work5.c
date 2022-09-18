8c38cbd520c539fd30a0026e725558a243a13e31
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-18 21:31:02 +0600

    Update work5.c

--- Complex ConditionDecision Making Practice/work5.c
+++ Complex ConditionDecision Making Practice/work5.c
@@ -8,17 +8,15 @@ int main(){
             if(basic_salary<=10000){
             HRA=(20*basic_salary)/100;
             DA=(80*basic_salary)/100;
-            printf("Your Gross Salary is: %.2f\n",gross_salary=basic_salary+HRA+DA );
-            }
-            else if(basic_salary<=20000){
+                        
+            }else if(basic_salary<=20000){
             HRA=(25*basic_salary)/100;
             DA=(90*basic_salary)/100;
-            printf("Your Gross Salary is: %.2f\n",gross_salary=basic_salary+HRA+DA );
-            }
-            else{
+                        
+            }else{
             HRA=(30*basic_salary)/100;
             DA=(95*basic_salary)/100;
-            printf("Your Gross Salary is: %.2f\n",gross_salary=basic_salary+HRA+DA );
             }
+            printf("Your Gross Salary is: %.2f\n",gross_salary=basic_salary+HRA+DA );
             return 0;
             }

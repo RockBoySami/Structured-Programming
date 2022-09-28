f161c0490b1aa889fc793d48681565bf58fefc5f
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-28 11:40:19 +0600

    Add files via upload

---
+++ Assignment on Over All Conditional/problem1005.c
@@ -0,0 +1,15 @@
+/*This  is the C program for calculating average number of a student*/
+#include<stdio.h>
+int main(){
+        float A,B,MEDIA;
+
+        printf("A: ");
+        scanf("%f",&A);
+
+        printf("B: ");
+        scanf("%f",&B);
+
+        printf("\nMEDIA = %f",MEDIA=(A+B)/2);
+
+        return 0;
+        }

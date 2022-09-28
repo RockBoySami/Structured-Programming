c9dd99abdb9f71f595ce3ac8928c15e73c122099
Md. Sami Alam <112261102+sami5671@users.noreply.github.com>
2022-09-28 13:46:56 +0600

    Update lab_work6.c

--- Lab Task 1/lab work 6/lab_work6.c
+++ Lab Task 1/lab work 6/lab_work6.c
@@ -1,3 +1,4 @@
+/*This is the C program for interchange the contents of C and D*/
 #include<Stdio.h>
 int main()
 {
@@ -7,8 +8,9 @@ int main()
 
      printf("Enter the value of D: ");
      scanf("%d", &D);
-
-
+     C = C+D; //c=30, d=20
+     D = C-D; //d=10, c=30
+     C = C-D; //c=20
 
      printf("The value of C: %d\n", C);
      printf("The value of D: %d\n", D);
